d8ff39ec1adbc754362b67412a5e8ae0724a8a19
FathimaPAshraf-dev <fathima@ssus.ac.in>
2026-06-11 05:55:58 +0000

    terminal

--- resources/views/pgfinalview.blade.php
+++ resources/views/pgfinalview.blade.php
@@ -799,7 +799,7 @@
 
    
 <tr hidden="">
-      <th scope="row">Application Printout</th>
+      <th scope="row">Application Prin</th>
     
       <td><div class="col-sm-4">
                       <form action="{{route('getpdf')}}" method="get" name="pdffrom">
